9af5eb631aecb4523f886b42b4dd015a5887f620
alexbutenko <alexandr.butenko@gmail.com>
2014-09-17 16:19:32 +1200

    + initial

--- TIFFWriter/ViewController.m
+++ TIFFWriter/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <ImageIO/ImageIO.h>
+#import <MobileCoreServices/UTCoreTypes.h>
 
 @interface ViewController ()
 
@@ -17,6 +19,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *img = [UIImage imageNamed:@"test_image.jpg"];
+    
+    float compression = 1.0; // Lossless compression if available.
+    int orientation = 0; // Origin is at bottom, left.
+    CFStringRef myKeys[3];
+    CFTypeRef   myValues[3];
+    CFDictionaryRef myOptions = NULL;
+    myKeys[0] = kCGImagePropertyOrientation;
+    myValues[0] = CFNumberCreate(NULL, kCFNumberIntType, &orientation);
+    myKeys[1] = kCGImagePropertyHasAlpha;
+    myValues[1] = kCFBooleanTrue;
+    myKeys[2] = kCGImageDestinationLossyCompressionQuality;
+    myValues[2] = CFNumberCreate(NULL, kCFNumberFloatType, &compression);
+    myOptions = CFDictionaryCreate( NULL, (const void **)myKeys, (const void **)myValues, 3,
+                                   &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0]; // Get documents folder
+    
+    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:@"test_image.tiff"];
+    
+    [self writeCGImage:img.CGImage toURL:[NSURL fileURLWithPath:filePath] withType:kUTTypeTIFF andOptions:myOptions];
+}
+
+- (void) writeCGImage: (CGImageRef) image toURL: (NSURL*) url withType: (CFStringRef) imageType andOptions: (CFDictionaryRef) options
+{
+    CGImageDestinationRef myImageDest = CGImageDestinationCreateWithURL((CFURLRef)url, imageType, 1, nil);
+    CGImageDestinationAddImage(myImageDest, image, options);
+    CGImageDestinationFinalize(myImageDest);
+    CFRelease(myImageDest);
 }
 
 - (void)didReceiveMemoryWarning {
